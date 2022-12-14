29deca29df4deb4112adf37d55ffab70e9e00b7e
Brandon H. Gomes <bhgomes@pm.me>
2022-12-14 17:46:37 -0500

    wip: commit save point
    Signed-off-by: Brandon H. Gomes <bhgomes@pm.me>

--- plonky2/src/plonk/circuit_data.rs
+++ plonky2/src/plonk/circuit_data.rs
@@ -259,7 +259,7 @@ pub struct ProverOnlyCircuitData<
 }
 
 /// Circuit data required by the verifier, but not the prover.
-#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct VerifierOnlyCircuitData<C: GenericConfig<D>, const D: usize> {
     /// A commitment to each constant polynomial and each permutation polynomial.
     pub constants_sigmas_cap: MerkleCap<C::F, C::Hasher>,
